6f1167c3e2c135d8c590b5fa89b945b8fbdb0103
sopan ishwar bhagwat <sopanbhagwat3107@gmail.com>
2025-09-17 14:01:35 +0000

    dates done

---
+++ datas.js
@@ -0,0 +1,14 @@
+let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleDateString()) // best 
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleString())
+// console.log(typeof mydate)
+
+// --- hum bhi khud date dal sakdte he
+
+let myCreatedDate = new Date(2026,1,31,5,3) // last mee time he 5hr 3 min
+console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate1 = new Date("01-31-2026") //month-date-year
+// console.log(myCreatedDate1.toLocaleString());
